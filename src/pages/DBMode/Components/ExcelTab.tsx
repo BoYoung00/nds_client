3d9ad652d9a0da04f84a95df43fd391585e00d76
BoYoung00 <qhdud20@naver.com>
2024-10-04 04:22:35 +0900

    10/04 Excel 탭 높이 수정

--- src/pages/DBMode/Components/ExcelTab.tsx
+++ src/pages/DBMode/Components/ExcelTab.tsx
@@ -67,32 +67,34 @@ const ExcelTab: React.FC = () => {
                     <section style={{borderLeft: `1px solid gray`, height: '100%'}}></section>
 
                     <section className={styles.fileContainer}>
-                        <div className={styles.description}>
-                            <h2>CSV Import</h2>
-                            <p>CSV 파일로 테이블에 데이터 추가하는 방법:</p>
-                            <p>
-                                1. 엑셀 가장 상단 행은 <span>테이블 행 이름과 동일</span>해야 합니다.
-                            </p>
-                            <p>
-                                2. 해당 시트의 이름은 항상 <span>"Sheet1"</span>으로 해주세요.
-                            </p>
-                            <p>
-                                3. 파일 선택을 눌러 Excel 파일을 첨부 후 <span>"테이블에 데이터 추가"</span>를 눌러주세요.
-                            </p>
-                        </div>
-                        <label className={styles.fileInputWrapper}>
-                            <p>Excel 파일 선택:</p>
-                            <input type="file" accept=".xls,.xlsx" onChange={handleFileChange} />
-                        </label>
-                        <div className={styles.importPreviewWrap}>
-                            <div className={styles.dataPreviewWrap}>
-                                <h3>Excel Data Preview</h3>
-                                <div className={styles.dataPreview}>
-                                    {tableViewData && displayExcelData(tableViewData)}
+                        <div className={styles.fileContainerTop}>
+                            <div className={styles.description}>
+                                <h2>CSV Import</h2>
+                                <p>CSV 파일로 테이블에 데이터 추가하는 방법:</p>
+                                <p>
+                                    1. 엑셀 가장 상단 행은 <span>테이블 행 이름과 동일</span>해야 합니다.
+                                </p>
+                                <p>
+                                    2. 해당 시트의 이름은 항상 <span>"Sheet1"</span>으로 해주세요.
+                                </p>
+                                <p>
+                                    3. 파일 선택을 눌러 Excel 파일을 첨부 후 <span>"테이블에 데이터 추가"</span>를 눌러주세요.
+                                </p>
+                            </div>
+                            <label className={styles.fileInputWrapper}>
+                                <p style={{marginTop: '1rem'}}>Excel 파일 선택:</p>
+                                <input type="file" accept=".xls,.xlsx" onChange={handleFileChange} />
+                            </label>
+                            <div className={styles.importPreviewWrap}>
+                                <div className={styles.dataPreviewWrap}>
+                                    <h3>Excel Data Preview</h3>
+                                    <div className={styles.dataPreview}>
+                                        {tableViewData && displayExcelData(tableViewData)}
+                                    </div>
                                 </div>
                             </div>
-                            <button className={styles.excelBut} onClick={handleFetchSaveCsvData}>테이블에 데이터 추가</button>
                         </div>
+                        <button className={styles.excelBut} onClick={handleFetchSaveCsvData}>테이블에 데이터 추가</button>
                     </section>
                 </main>
             </div>
